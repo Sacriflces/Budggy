da2808925bc7cee14672e3fe467ab7ec37943f10
Sacriflces <Jmhservice@gmail.com>
2018-11-11 16:09:46 -0500

    finish initialize work on Bins and tested them. I started working on the Budget class, but I may need to research more on API , controller accessiblility   and databinding before I complete those.

--- BudggyUWP/BudggyTestClassLibrary/Bin.cs
+++ BudggyUWP/BudggyTestClassLibrary/Bin.cs
@@ -15,26 +15,91 @@ namespace Budggy
         double Balance { get; set; }
         double Percentage { get; set; }
 
-        public void addExpense(double value, string destr, DateTime date)
+        double MinimumBalance { get; set; }
+        double GoalBalance { get; set; }
+        double Mulitplier { get; set; }
+
+        public void AddExpense(double value, string destr, DateTime date)
         {
             Expense expense = new Expense(value, destr, date);
             Expenses.Add(expense);
+            CalcBalance();
+            OrganizeExpensesByDate();
         }
 
-        public void addIncome(double value, string destr, DateTime date)
+        public void DeleteExpense(int index)
+        {
+            Expenses.RemoveAt(index);
+            CalcBalance();
+            OrganizeExpensesByDate();
+        }   
+
+        public int ExpenseSize()
+        {
+            return Expenses.Count();
+        }
+
+        public void AddIncome(double value, string destr, DateTime date)
         {
             Income income = new Income(value, destr, date);
-            Incomes.Add(income);
+            Incomes.Add(income);            
+            CalcBalance();
+            OrganizeIncomesByDate();
         }
 
-        public double getEValue(int index)
+        public void DeleteIncome(int index)
+        {
+            Incomes.RemoveAt(index);
+            CalcBalance();
+            OrganizeIncomesByDate();
+        }
+
+        public int IncomeSize()
+        {
+            return Incomes.Count();
+        }
+
+        public double GetEValue(int index)
         {
             return Expenses[index].Value;
         }
 
-        public double getIValue(int index)
+        public double GetIValue(int index)
         {
             return Incomes[index].Value;
+        }
+        
+        public double GetBalance()
+        {
+            return Balance;
+        }
+
+        public double CalcBalance()
+        {
+            double sum = 0;
+
+            foreach(Income inc in Incomes)
+            {
+                sum += inc.Value;
+            }
+
+            foreach(Expense exp in Expenses)
+            {
+                sum -= exp.Value;
+            }
+
+            Balance = sum;
+            return Balance;
+        }
+
+        public void OrganizeIncomesByDate()
+        {            
+            Incomes.Sort((x, y) => DateTime.Compare(x.Date, y.Date));            
+        }
+
+        public void OrganizeExpensesByDate()
+        {
+           Expenses.Sort((x, y) => DateTime.Compare(x.Date, y.Date));
         }
 
     }
