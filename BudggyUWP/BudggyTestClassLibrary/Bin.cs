3f3d188515845301ffc84b079b04c3e41c053247
Sacriflces <Jmhservice@gmail.com>
2018-11-18 12:25:57 -0500

    Started building out the Budget methods

--- BudggyUWP/BudggyTestClassLibrary/Bin.cs
+++ BudggyUWP/BudggyTestClassLibrary/Bin.cs
@@ -4,20 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Budggy
+namespace BudggyTestClassLibrary
 {
    public class Bin
     {
-        List<Expense> Expenses = new List<Expense>();
-        List<Income> Incomes = new List<Income>();
+        internal List<Expense> Expenses = new List<Expense>();
+        internal List<Income> Incomes = new List<Income>();
         string Description { get; set; }
-        string Name { get; set; }
+        internal string Name { get; set; }
         double Balance { get; set; }
         double Percentage { get; set; }
 
         double MinimumBalance { get; set; }
         double GoalBalance { get; set; }
-        double Mulitplier { get; set; }
+        double Multiplier { get; set; }
+
+        public Bin(string name, string description, double percentage, double minimumBalance, double goalBalance, double multiplier)
+        {
+            Name = name;
+            Description = description;
+            Percentage = percentage;
+            MinimumBalance = minimumBalance;
+            GoalBalance = goalBalance;
+            Multiplier = multiplier;
+        }
+
+        public Bin(string name, string description, double percentage)
+        {
+            Name = name;
+            Description = description;
+            Percentage = percentage;          
+        }
+
 
         public void AddExpense(double value, string destr, DateTime date)
         {
@@ -94,7 +112,12 @@ namespace Budggy
 
         public void OrganizeIncomesByDate()
         {            
-            Incomes.Sort((x, y) => DateTime.Compare(x.Date, y.Date));            
+            Incomes.Sort((x, y) => DateTime.Compare(x.Date, y.Date));                       
+        }
+
+        public void OrganizeIncomesByValue()
+        {
+            Incomes.Sort((x, y) => DCompare(x.Value, y.Value));
         }
 
         public void OrganizeExpensesByDate()
@@ -102,5 +125,16 @@ namespace Budggy
            Expenses.Sort((x, y) => DateTime.Compare(x.Date, y.Date));
         }
 
+        public void OrganizeExpensesByValue()
+        {
+            Expenses.Sort((x, y) => DCompare(x.Value, y.Value));
+        }
+
+        int DCompare(double x, double y)
+        {
+            if (x - y > 0) return 1;
+            else if (x - y < 0) return -1;
+            else return 0;
+        }
     }
 }
--- BudggyUWP/BudggyTestClassLibrary/Expense.cs
+++ BudggyUWP/BudggyTestClassLibrary/Expense.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Budggy
+namespace BudggyTestClassLibrary
 {
     internal class Expense : Income
     {
