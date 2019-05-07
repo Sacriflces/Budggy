6677990a4f406f422e44b8437eb869d0b28f81e8
Sacriflces <35205308+Sacriflces@users.noreply.github.com>
2019-05-07 16:32:04 -0400

    Uhh before I change ALL doubles to decimal lol..

--- BudggyUWP/Budggy/Bin.cs
+++ BudggyUWP/Budggy/Bin.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,23 +25,10 @@ namespace Budggy
                 PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
         }
 
-        // internal List<Expense> Expenses = new List<Expense>();
-        //internal List<Income> Incomes = new List<Income>();
+        public ObservableCollection<Drawer> Drawers = new ObservableCollection<Drawer>();
 
-        public class Drawer : INotifyPropertyChanged
-        {
-            #region INotifyPropertyChanged Members
 
-            public event PropertyChangedEventHandler PropertyChanged;
 
-            #endregion
-
-            void OnPropertyChange(string propertyName)
-            {
-                if (PropertyChanged != null)
-                    PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
 
         private string _description;
         public string Description
@@ -231,5 +219,119 @@ namespace Budggy
             else if (x - y < 0) return -1;
             else return 0;
         }
+    }
+
+    public class Drawer : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        void OnPropertyChange(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private decimal _maximum;
+        public decimal Maximum
+        {
+            get { return _maximum; }
+            set
+            {
+                _maximum = value;
+                OnPropertyChange("Maximum");
+            }
+        }
+
+        private decimal Max;
+
+        private decimal _currentSpent;
+        public decimal CurrentSpent
+        {
+            get { return _currentSpent; }
+            set
+            {
+                _currentSpent = value;
+                OnPropertyChange("CurrentSpent");
+            }
+        }
+
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChange("Name");
+            }
+        }
+
+        private int _month;
+        public int Month
+        {
+            get { return _month; }
+            set
+            {
+                _month = value;
+                OnPropertyChange("Month");
+            }
+        }
+
+        private int _year;
+        public int Year
+        {
+            get { return _year; }
+            set
+            {
+                _year = value;
+                OnPropertyChange("Month");
+            }
+        }
+
+        decimal[] prevSpent = new decimal[12];
+        private int index;
+        public bool rollOver;
+
+        public Drawer()
+        {
+
+        }
+        
+        public Drawer(string name, decimal maximum, DateTime date)
+        {
+            Name = name;
+            Maximum = maximum;
+            Max = maximum;
+            CurrentSpent = 0;
+            Month = date.Month;
+            Year = date.Year;
+            rollOver = false;
+            index = 0;
+        }
+
+        public void Refresh()
+        {
+            if (rollOver)
+            {
+                Maximum = Max + (Maximum - CurrentSpent);
+            }
+            else
+                Maximum = Max;
+
+            prevSpent[index++] = CurrentSpent;
+            index %= prevSpent.Length;
+            Month = DateTime.Now.Month;
+            Year = DateTime.Now.Year;
+        }
+
+        public void AddExpense()
+        {
+
+        }
+
     }
 }
